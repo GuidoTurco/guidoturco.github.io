1225d0545e808b8801e2abed645032b475658164
GuidoTurco <guidoturco@protonmail.com>
2025-12-26 12:09:28 +0100

    Update generate-feed.js

--- generate-feed.js
+++ generate-feed.js
@@ -98,6 +98,7 @@ const now = new Date();
 const lastBuild = toRFC822(now);
 
 let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="feed.xsl"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
     <title>Notariqon – casa d'Arno</title>
